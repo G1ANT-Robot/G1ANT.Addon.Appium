36c4a43be2540bbb52363d352ddfaaf7d9ea3968
Przemysław Gwarek <przemyslaw.gwarek@g1ant.com>
2019-07-02 14:55:45 +0200

    Added appium.close command

---
+++ G1ANT.Appium/Commands/CloseCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using G1ANT.Language;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.Appium
+{
+    [Command(Name = "appium.close", Tooltip = "This command closes appium session")]
+    public class CloseCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required =true,Tooltip = "Keys to be sent to element")]
+            public TextStructure KeyCode { get; set; } = new TextStructure("");
+        }
+
+        public CloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            var driver = OpenCommand._driver;
+            driver.Quit();
+        }
+    }
+}
+
